5e89aa0717ac2fea3799cdeba0cd7fdb1eb045b2
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-10-13 23:25:31 -0500

    🔥 Project initialized

---
+++ Assets/Script/ARPlacementURP.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementURP : MonoBehaviour
+{
+    // We need orientation to the AR Camera
+    public ARSessionOrigin sessionOrigin;
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+    private GameObject spawnedObject;
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Need to update placement indicator, placement pose and spawn 
+    void Update()
+    {
+        if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (spawnedObject == null && placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = sessionOrigin.camera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f)); // Specifically from ARSession origin camera
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            PlacementPose = hits[0].pose;
+            // Need to align the placement pose same as the camera's orientation
+            var cameraForward = sessionOrigin.camera.transform.forward;
+            var cameraOrientation = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            PlacementPose.rotation = Quaternion.LookRotation(cameraOrientation);
+        }
+    }
+
+    void ARPlaceObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+    }
+}
+
